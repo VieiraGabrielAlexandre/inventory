69284059cb5c68b1ba5f366294b259224259b478
Gabriel Vieira <gabrielvieira840@gmail.com>
2022-04-02 17:03:01 -0300

    connect with db postgres

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
 var templates = template.Must(template.ParseGlob("templates/*.html"))
+
+func conectDB() *sql.DB {
+	db, err := sql.Open("postgres", "user=postgres password=root dbname=inventory host=localhost port=5432 sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
 
 type Produto struct {
 	Nome       string
@@ -20,15 +31,34 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{Nome: "Caneta", Descricao: "Caneta Bic Preta", Preco: 1.79, Quantidade: 10},
-		{Nome: "Caderno", Descricao: "Caderno Escolar", Preco: 3.12, Quantidade: 25},
-		{Nome: "Lapis", Descricao: "Lapis Preto", Preco: 1.79, Quantidade: 15},
-		{Nome: "Borracha", Descricao: "Borracha Azul", Preco: 1.79, Quantidade: 5},
+	db := conectDB()
+	lista, err := db.Query("select * from produtos")
+
+	if err != nil {
+		panic(err.Error())
 	}
 
-	err := templates.ExecuteTemplate(w, "Index", produtos)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	produtos := []Produto{}
+	p := Produto{}
+
+	for lista.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = lista.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
 	}
+
+	templates.ExecuteTemplate(w, "Index", produtos)
+	defer db.Close()
 }
